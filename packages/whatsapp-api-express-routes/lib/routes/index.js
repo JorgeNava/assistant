f38579ea310d7616759197d2cfbaacbe144d1ab3
Jorge Nava <jorge.nava@boats.com>
2024-02-21 13:24:44 -0600

    WA healtcheck

--- packages/whatsapp-api-express-routes/lib/routes/index.js
+++ packages/whatsapp-api-express-routes/lib/routes/index.js
@@ -4,6 +4,11 @@ const whatsappRoutes = express.Router({ mergeParams: true });
 
 const { whatsappController } = require('../controller');
 
+whatsappRoutes.get('healthcheck', (req, res) => {
+  const verify_token = process.env.WHATSAPP_VERIFY_TOKEN;
+  console.log('whatsapp working');
+  res.status(200);
+});
 whatsappRoutes.get('webhook', whatsappController.verifyWebhook);
 whatsappRoutes.post('webhook', whatsappController.receiveMessage);
 
